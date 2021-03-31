185e7880c64d4a67404301ed81712e659d8715b2
nathan-garrett <81236890+nathan-garrett@users.noreply.github.com>
2021-03-31 21:56:14 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API = process.env.REACT_APP_API_URL || 'https://back-end-server-123.herokuapp.com';
+export const API = process.env.REACT_APP_API_URL || 'https://back-end-server-123.herokuapp.com/api';
 
